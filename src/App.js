24bca84d0ea375faa6dcda781fd4e696d3fd3a28
unknown <mmluwang4@gmail.com>
2021-12-25 21:11:53 +0530

    updated menu-items to navigate with useNavigate HOC

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
+import React from 'react';
+import {Route, Routes, useLocation } from 'react-router-dom';
 
 import './App.css';
+
 import HomePage from './pages/homepage/homepage.component';
+
+const HatsPage = (props) =>{
+  let location = useLocation()
+  console.log("props hats", props)
+  console.log("loction -> ", location)
+return (
+  <div>
+    <h1>HATS PAGE </h1>
+  </div>
+);
+}
 
 function App() {
   return (
     <div>
-     <HomePage/>
+      <Routes>
+        <Route path='/' element={<HomePage/>} />
+        <Route path='/hats' element={<HatsPage/>} />
+      </Routes>
     </div>
   );
 }
--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -1,10 +1,14 @@
 import Directory from '../../components/directory/directory.component';
 import './homepage.styles.scss';
 
-const HomePage = () => (
+const HomePage = (props) =>{
+    console.log("props home", props)
+ return(
+
     <div className="homepage">
         <Directory />
     </div>
 );
+ }
 
 export default HomePage;
